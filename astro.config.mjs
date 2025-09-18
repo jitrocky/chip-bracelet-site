81691bd8b8d1dc31d38e91f1ed08e38ae8d58d02
jitrocky <167745709+jitrocky@users.noreply.github.com>
2025-09-18 13:13:13 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://jitrocky.github.io/chip-bracelet-site'
+  base: '/chip-bracelet-site/',       // ✅ 关键：告诉 Astro 资源在子路径下
+  integrations: [tailwind()],
 });
